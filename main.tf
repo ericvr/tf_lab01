aa2d61b2a52287e1d47dd2cb4bddfa87c060c459
Gonzalo González <gonzalo.gonzalez@afirma.solutions>
2022-08-15 18:01:50 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ resource "docker_image" "nginx" {
 
 resource "docker_container" "nginx" {
   image = docker_image.nginx.latest
-  name  = "theironhidex"
+  name  = "gonzafirma"
   ports {
     internal = 80
     external = 8000
