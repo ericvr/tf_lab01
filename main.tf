18c37e44cd887d40572c43ad1740accb6ff40135
Gonzalo González <gonzalo.gonzalez@afirma.solutions>
2022-08-16 16:17:51 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,6 +7,9 @@ terraform {
   }
 }
 
+variable "reponame" {}
+variable "container_port" {}
+
 provider "docker" {}
 
 resource "docker_image" "nginx" {
@@ -16,9 +19,9 @@ resource "docker_image" "nginx" {
 
 resource "docker_container" "nginx" {
   image = docker_image.nginx.latest
-  name  = "gonzafirma"   // Cambiarla dinámicamente por la Variable env.DOCKER_REPO que está en el Jenkins
+  name  = "${var.reponame}"   // Cambiarla dinámicamente por la Variable env.DOCKER_REPO que está en el Jenkins
   ports {
     internal = 80
-    external = 80  // Cambiarla dinámicamente por la variable CONTAINER_PORT que está en el Jenkins.
+    external = ${var.container_port}  // Cambiarla dinámicamente por la variable CONTAINER_PORT que está en el Jenkins.
   }
 }
