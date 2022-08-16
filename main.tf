e3f097bc9b0b92ec247f6fe47b5b0fab3acd324c
Gonzalo González <gonzalo.gonzalez@afirma.solutions>
2022-08-16 16:23:54 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,6 +22,6 @@ resource "docker_container" "nginx" {
   name  = "${var.reponame}"   // Cambiarla dinámicamente por la Variable env.DOCKER_REPO que está en el Jenkins
   ports {
     internal = 80
-    external = ${var.container_port}  // Cambiarla dinámicamente por la variable CONTAINER_PORT que está en el Jenkins.
+    external = "${var.container_port}"  // Cambiarla dinámicamente por la variable CONTAINER_PORT que está en el Jenkins.
   }
 }
